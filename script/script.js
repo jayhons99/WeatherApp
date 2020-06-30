04306801a5da3e2a027ab41c9dfeb17e0f00dec0
Jhon Sisneros <js62207@uga.edu>
2020-06-30 17:51:31 -0400

    Included an api from OpenWeatherMap in script.

--- script/script.js
+++ script/script.js
@@ -6,7 +6,16 @@ window.addEventListener("load", ()=> {
         navigator.geolocation.getCurrentPosition(pos => {
             long = pos.coords.longitude;
             lat = pos.coords.latitude;
-        })
+            /* API retrieved from https://openweathermap.org */ 
+            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=de881718d16b4ed2ac9774f9d99c81ff`;
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+        });
     } else {
         h1.textContent = "Unable to find location, please try again. Browser"
         + "may not be supported";
