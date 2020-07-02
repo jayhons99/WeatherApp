726f7aecf5b88b5f07b9a068bdb589933b08d3ab
Jhon Sisneros <js62207@uga.edu>
2020-07-02 12:59:31 -0400

    Successfully retrieve weather data and coded in icons.

--- script/script.js
+++ script/script.js
@@ -2,18 +2,35 @@
 window.addEventListener("load", ()=> {
     let long;
     let lat;
+    let desc = document.querySelector(".temperature-description");
+    let cityName = document.querySelector(".location-city");
+    let degree = document.querySelector(".temperature-degree");
+    let icon = document.querySelector(".icon");
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(pos => {
             long = pos.coords.longitude;
             lat = pos.coords.latitude;
             /* API retrieved from https://openweathermap.org */ 
-            const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=de881718d16b4ed2ac9774f9d99c81ff`;
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=de881718d16b4ed2ac9774f9d99c81ff`;
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
+                    /* Retrieves temperature, timezone, and description of weather in area. Add in more features later. */
                     console.log(data);
+                    const {temp} = data.main; 
+                    const city = data.name;
+                    const icon = data.weather[0].icon;
+                    const description = data.weather[0].description;
+                    const kelvinToF = ((9/5) * (temp - 273) + 32).toFixed(0); 
+                    /* Set DOM elements to retrieved data. */
+                    const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+                    document.getElementById("icon").src = iconURL;
+                    degree.textContent = kelvinToF;
+                    cityName.textContent = city; 
+                    desc.textContent = capitalize(description);
                 });
         });
     } else {
@@ -21,3 +38,8 @@ window.addEventListener("load", ()=> {
         + "may not be supported";
     }
 });
+
+/* Function that capitalizes first character in string when appropriate. */
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
